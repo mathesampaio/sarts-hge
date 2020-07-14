ac7a1cb2b9dd06b5b23727286a9e76046ab621a8
mathesampaio <50926169+mathesampaio@users.noreply.github.com>
2020-07-14 18:31:49 -0300

    teste1

--- sarts/settings.py
+++ sarts/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w%wv1(20#&2b1rhbyv$lkus1g=4k!($sx22!u=9wb^j=9ptdao'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
